c428a09bfdb9a90a6448fffd9dbfd378a129e8cc
yisha <chenchen9037@126.com>
2022-07-29 21:59:40 +0800

    20220729

---
+++ py4office/py-tutorial/test11.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv('py4office/py-pandas/data/property-data.csv')
+# print(data)
+
+data2 = pd.read_json('py4office/py-pandas/data/nested_list.json')
+print(data2)
